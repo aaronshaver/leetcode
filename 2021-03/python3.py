2a1b3827744a57631c59a4f2bd0248a8c32924ca
Aaron Shaver <aaronshaverpdx@gmail.com>
2021-03-30 15:36:47 -0700

    Python

--- 2021-03/python3.py
+++ 2021-03/python3.py
@@ -1,6 +1,17 @@
 
 
 # -----------------------------------------------------------------------------
+# https://leetcode.com/problems/running-sum-of-1d-array/submissions/
+class Solution:
+    def runningSum(self, nums):
+        running = 0
+        output = []
+        for i in range(len(nums)):
+            running += nums[i]
+            output.append(running)
+        return output
+
+# -----------------------------------------------------------------------------
 # https://leetcode.com/problems/two-sum/submissions/
 class Solution:
     def twoSum(self, nums, target):
