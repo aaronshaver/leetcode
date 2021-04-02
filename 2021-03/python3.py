2d2a46d3a71bed93fce7ab1c1073f3811a37ea6d
Aaron Shaver <aaronshaverpdx@gmail.com>
2021-04-02 15:26:45 -0700

    3 easy ones

--- 2021-03/python3.py
+++ 2021-03/python3.py
@@ -1,4 +1,35 @@
 
+# -----------------------------------------------------------------------------
+# https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/submissions/
+class Solution:
+    def kidsWithCandies(self, candies, extraCandies):
+        output = []
+        highest_candy = max(candies)
+        
+        for c in candies:
+            if (c >= highest_candy) or (c + extraCandies >= highest_candy):
+                output.append(True)
+            else:
+                output.append(False)
+        
+        return output
+
+# -----------------------------------------------------------------------------
+# https://leetcode.com/problems/shuffle-the-array/submissions/
+class Solution:
+    def shuffle(self, nums, n):
+        output = []
+        for i in range(n):
+            output.append(nums[i])
+            output.append(nums[i+n])
+        
+        return output
+
+# -----------------------------------------------------------------------------
+# https://leetcode.com/problems/defanging-an-ip-address/submissions/
+class Solution:
+    def defangIPaddr(self, address):
+        return address.replace('.', '[.]')
 
 # -----------------------------------------------------------------------------
 # https://leetcode.com/problems/running-sum-of-1d-array/submissions/
