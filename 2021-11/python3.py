3fc074d4104c64aabc0691f6423eac6c73746a8e
Aaron Shaver <aaronshaverpdx@gmail.com>
2021-11-02 15:18:09 -0700

    easy one to warm up the brain

---
+++ 2021-11/python3.py
@@ -0,0 +1,9 @@
+# -----------------------------------------------------------------------------
+# https://leetcode.com/problems/build-array-from-permutation/submissions/
+
+class Solution:
+    def buildArray(self, nums):
+        output = []
+        for i in range(len(nums)):
+            output.append(nums[nums[i]])
+        return output
