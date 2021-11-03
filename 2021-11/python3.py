d298ad3483b58e489531ea684fb728c4a3bf5f44
Aaron Shaver <aaronshaverpdx@gmail.com>
2021-11-03 15:39:43 -0700

    a few more; XOR one actually required a little thought, ha

--- 2021-11/python3.py
+++ 2021-11/python3.py
@@ -1,3 +1,18 @@
+
+# -----------------------------------------------------------------------------
+# https://leetcode.com/problems/decode-xored-array/submissions/
+
+class Solution:
+    def decode(self, encoded: List[int], first: int) -> List[int]:
+        output = [first]
+        left = first
+        for num in encoded:
+            new = left ^ num  # xor next encoded num with last known unencoded
+            output.append(new)
+            left = new  # new last known unencoded
+            
+        return output
+
 # -----------------------------------------------------------------------------
 # https://leetcode.com/problems/build-array-from-permutation/submissions/
 
@@ -7,3 +22,23 @@ class Solution:
         for i in range(len(nums)):
             output.append(nums[nums[i]])
         return output
+
+# -----------------------------------------------------------------------------
+# https://leetcode.com/problems/concatenation-of-array/submissions/
+
+class Solution:
+    def getConcatenation(self, nums):
+        return nums * 2
+
+# -----------------------------------------------------------------------------
+# https://leetcode.com/problems/final-value-of-variable-after-performing-operations/submissions/
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        x = 0
+        for op in operations:
+            if op in ["X++", "++X"]:
+                x += 1
+            else:
+                x -= 1
+        return x
