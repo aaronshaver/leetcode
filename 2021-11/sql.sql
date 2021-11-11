c628fd234d8dfaf7fe2b0475fc2eabaaae189a67
Aaron Shaver <aaronshaverpdx@gmail.com>
2021-11-11 15:08:22 -0800

    practicing table joins in SQL

--- 2021-11/sql.sql
+++ 2021-11/sql.sql
@@ -1,3 +1,10 @@
+-- --------------------------------------------------------------------------
+-- https://leetcode.com/problems/combine-two-tables/submissions/
+
+select firstName, lastName, city, state
+from Person p
+left join Address a
+on p.personId = a.personId
 
 -- --------------------------------------------------------------------------
 -- https://leetcode.com/problems/swap-salary/solution/
