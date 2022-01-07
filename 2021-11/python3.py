d7a5dd499002a2001c936314fed124fc95a1437e
Aaron Shaver <aaronshaverpdx@gmail.com>
2022-01-06 16:43:00 -0800

    easy naive solution, but optimizing it proves interesting

--- 2021-11/python3.py
+++ 2021-11/python3.py
@@ -1,5 +1,32 @@
 
 
+# https://leetcode.com/problems/count-number-of-pairs-with-absolute-difference-k/submissions/
+
+# my solution, which is slow at O(n^2)
+class Solution:
+    def countKDifference(self, nums: List[int], k: int) -> int:
+        count = 0
+        for i in range(len(nums)):
+            for j in range (i + 1, len(nums)):
+                if abs(nums[i] - nums[j]) == k:
+                    count += 1
+        
+        return count
+
+# slick, fast, and easy to read solution I saw in the Discussions area:
+class Solution:
+    def countKDifference(self, nums: List[int], k: int) -> int:
+        d = {}
+        count = 0
+        
+        for num in nums:
+            d[num] = d.get(num, 0) + 1
+            count += d.get(num - k, 0)
+            count += d.get(num + k, 0)
+                
+        return count
+
+
 # https://leetcode.com/problems/find-center-of-star-graph/submissions/
 
 class Solution:
