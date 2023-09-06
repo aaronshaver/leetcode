0421f14143eedcef28a003bf773287135d7a0b06
Aaron Shaver <aaronshaverpdx@gmail.com>
2023-09-05 17:44:08 -0700

    this one was interesting

--- 2023-09/python3.py
+++ 2023-09/python3.py
@@ -13,6 +13,45 @@
 # ---------------------------------------------------------------------------
 # ^^^^ template ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
+# ---------------------------------------------------------------------------
+# url: https://leetcode.com/problems/greatest-common-divisor-of-strings/?envType=study-plan-v2&envId=leetcode-75
+
+# discuss tab solution
+# this is actually GPT-4's solution; quite clever:
+# time -- I was incorrect:
+# O(n*m) because we loop through both strings once; "The string
+# multiplication and comparison take time proportional to the lengths of both strings."
+#
+# space -- I was correct; GPT-4 wants to use this notation:, O(min(n, m))
+# more commonly you'd probably just have O(n) and note that n is the shorter string
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        for i in range(min(len(str1), len(str2)), 0, -1):
+            chunk = str1[:i]
+            if str1[:i] == str2[:i]:
+                if str1.replace(chunk, "") == "" and str2.replace(chunk, "") == "":
+                    return chunk
+        return ""
+
+# my solution
+# time: O(n)?
+# space: O(m) where m is the shorter of the two strings??
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        size = 0
+        candidate = ""
+        while True:
+            size += 1
+            if size > len(str2) or size > len(str1):
+                return candidate
+            chunk = str1[0:size]
+            if chunk * (len(str1) // len(chunk)) == str1 and \
+            chunk * (len(str2) // len(chunk)) == str2:
+                candidate = chunk
+        return candidate
+# ---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
 # url: https://leetcode.com/problems/merge-strings-alternately/?envType=study-plan-v2&envId=leetcode-75
 
 # discuss tab solution
@@ -45,3 +84,4 @@ class Solution:
             longest_word = word1 if len(word1) > len(word2) else word2
             output += longest_word[shortest_length:]
         return "".join(output)
+# ---------------------------------------------------------------------------
