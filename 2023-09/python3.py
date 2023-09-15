bcdaf5c1007bc2236cf5bd91461b5e9ddfdba877
Aaron Shaver <aaronshaverpdx@gmail.com>
2023-09-15 11:51:08 -0700

    add another solution that has O(1) space complx

--- 2023-09/python3.py
+++ 2023-09/python3.py
@@ -17,6 +17,27 @@
 # url: https://leetcode.com/problems/valid-anagram/description/
 
 # (notes from LeetCode Solutions tab and/or ChatGPT)
+# here's one from Solutions tab that has O(1) space complexity assuming we're
+# only dealing with lowercase English letters, which in this case we are
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count = [0] * 26
+
+        # Count the frequency of characters in string s
+        for x in s:
+            count[ord(x) - ord('a')] += 1
+
+        # Decrement the frequency of characters in string t
+        for x in t:
+            count[ord(x) - ord('a')] -= 1
+
+        # Check if any character has non-zero frequency
+        for val in count:
+            if val != 0:
+                return False
+
+        return True
+
 # here's an option that uses an imported library:
 from collections import Counter
 
