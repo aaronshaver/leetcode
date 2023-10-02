05c119a4d0185bd26d4a624e95f5abff3d3033e9
Aaron Shaver <aaronshaverpdx@gmail.com>
2023-10-02 12:12:10 -0700

    WIP

--- 2023-09/python3.py
+++ 2023-09/python3.py
@@ -14,6 +14,27 @@
 # ^^^^ template ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 # ---------------------------------------------------------------------------
+# url: https://leetcode.com/problems/balanced-binary-tree/description/
+
+# (notes from LeetCode Solutions tab and/or ChatGPT)
+
+
+# (my solution)
+# time:
+# space:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root):
+
+
+# ---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
 # url: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
 
 # (notes from LeetCode Solutions tab and/or ChatGPT)
