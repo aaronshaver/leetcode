84ec8338e9fdd866312706bd802e6f3f549e8a11
Aaron Shaver <aaronshaverpdx@gmail.com>
2023-10-06 11:44:16 -0700

    solved after a hint; still tweaking it

--- 2023-09/python3.py
+++ 2023-09/python3.py
@@ -22,8 +22,28 @@
 # (my solution)
 # time:
 # space:
+class MyQueue:
+    def __init__(self):
+        self.list1 = []  # for push()
+        self.list2 = []  # for peek(), pop()
 
+    def push(self, x: int) -> None:
+        self.list1.append(x)
 
+    def pop(self) -> int:
+        if not self.list2:
+            while self.list1:
+                self.list2.append(self.list1.pop())
+        return self.list2.pop()
+
+    def peek(self) -> int:
+        if not self.list2:
+            while self.list1:
+                self.list2.append(self.list1.pop())
+        return self.list2[-1]
+
+    def empty(self) -> bool:
+        return len(self.list2) == 0 and len(self.list1) == 0
 # ---------------------------------------------------------------------------
 
 # ---------------------------------------------------------------------------
