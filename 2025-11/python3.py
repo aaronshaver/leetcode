823813605931a117169ea8b02540fa80175a43af
Aaron Shaver <aaronshaverpdx@gmail.com>
2025-11-06 16:45:28 -0800

    u

--- 2025-11/python3.py
+++ 2025-11/python3.py
@@ -23,7 +23,7 @@
 # time:
 # space:
 class Solution:
-    def buildArray(self, nums: List[int]) -> List[int]:
+    def buildArray(self, nums):
         ans = []
         for i in range(len(nums)):
             ans.append(nums[nums[i]])
