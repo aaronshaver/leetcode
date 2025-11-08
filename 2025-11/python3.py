dcce72bb5a36e89b739b98137af9e4e901afaa50
Aaron Shaver <aaronshaverpdx@gmail.com>
2025-11-07 16:10:00 -0800

    u

--- 2025-11/python3.py
+++ 2025-11/python3.py
@@ -17,11 +17,23 @@
 # url: https://leetcode.com/problems/build-array-from-permutation/
 
 # (solution and/or notes from LeetCode Solutions tab and/or an AI model)
+# this is space complexity o(1) as it permutes the list in-place
+# the key insight is to realize that to achieve o(1) space, you must not overwrite
+# the original value until you're ready; you have to encode both the original value
+# and the new value in one "container" that is the single integer
+class Solution:
+    def buildArray(self, nums):
+        n = len(nums)
+        for i in range(n):
+            nums[i] = nums[i] + (nums[nums[i]] % n) * n
+        for i in range(n):
+            nums[i] = nums[i] // n
 
+        return nums
 
 # (my solution)
-# time:
-# space:
+# time: o(n)
+# space: o(n) -- this includes the list comprehension version
 class Solution:
     def buildArray(self, nums):
         ans = []
