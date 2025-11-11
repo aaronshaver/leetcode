2104e7b83da4f23b9a7406eec254d26b5f2ffba3
Aaron Shaver <aaronshaverpdx@gmail.com>
2025-11-10 16:20:00 -0800

    u

--- 2025-11/python3.py
+++ 2025-11/python3.py
@@ -21,6 +21,10 @@
 # the key insight is to realize that to achieve o(1) space, you must not overwrite
 # the original value until you're ready; you have to encode both the original value
 # and the new value in one "container" that is the single integer
+#
+# and the constraint than all numbers < n means we can reliably use modulo or
+# floor division to get either the old or new values (because e.g. the old value
+# is a remainder that will always be thrown away with // division)
 class Solution:
     def buildArray(self, nums):
         n = len(nums)
