d7dc7d1ab245494db678a2ba121000f31a37847f
Aaron Shaver <aaronshaverpdx@gmail.com>
2026-09-17 16:24:04 -0700

    get ready for a new round with new folder/etc.

---
+++ 2026-09/python3.py
@@ -0,0 +1,15 @@
+# vvvv template vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+# ---------------------------------------------------------------------------
+# url:
+
+# (solution and notes from LeetCode Solutions tab and/or AI model)
+
+
+# (my solution)
+# time:
+# space:
+
+
+# ---------------------------------------------------------------------------
+# ^^^^ template ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
--- README.md
+++
@@ -1,3 +0,0 @@
-# LeetCode
-
-Storage for my LeetCode solutions
