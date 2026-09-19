0457e70075dc5cca5886de210139a74f579431ed
Aaron Shaver <aaronshaverpdx@gmail.com>
2026-09-18 17:00:01 -0700

    u

--- 2026-09/python3.py
+++ 2026-09/python3.py
@@ -17,11 +17,23 @@
 # url: https://leetcode.com/problems/two-sum/description/
 
 # (solution and notes from LeetCode Solutions tab and/or AI model)
+# I half-remembered the solution but I got a hint from the LLM to get to the
+# time complexity of O(n)
+
 
 
 # (my solution)
-# time:
-# space:
-
+# time: O(n^2)
+# space: O(n)
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        i = 0
+        for first in nums:
+            j = i + 1
+            for second in nums[i + 1:]:
+                if first + second == target:
+                    return [i, j]
+                j += 1
+            i += 1
 
 # ---------------------------------------------------------------------------
