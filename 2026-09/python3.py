d07e1f2dbc74ef4584813fc661ce14e946733252
Aaron Shaver <aaronshaverpdx@gmail.com>
2026-09-21 16:51:14 -0700

    O(n) time complexity using stack-like list object and append() and pop() O(1) operations

--- 2026-09/python3.py
+++ 2026-09/python3.py
@@ -19,7 +19,8 @@
 # (solution and notes from LeetCode Solutions tab and/or AI model)
 #
 # after a couple small hints, like using a list as a stack, and realizing
-# slicing off the last two entries in the list is O(n)
+# slicing off the last two entries in the list copies the whole list thus
+# risking O(n^2) again (doing an O(n) for every element in the string)
 class Solution:
     def isValid(self, s: str) -> bool:
         if len(s) < 2 or s[0] in [']', '}', ')']:
@@ -34,20 +35,6 @@ class Solution:
                     parens.pop()  # remove the closed pair
                     parens.pop()  # remove the closed pair
         return not parens  # every pair closed and remove
-class Solution:
-    def isValid(self, s: str) -> bool:
-        if len(s) < 2 or s[0] in [']', '}', ')']:
-            return False
-
-        parens = []
-        parensMap = { ')': '(', ']': '[', '}': '{'}
-        for char in s:
-            parens.append(char)
-            if len(parens) > 1 and char in [']', '}', ')']:
-                if parens[-2] == parensMap[char]:  # if two back is opening paren
-                    parens = parens[:-2]  # remove the closed pair
-        return not parens  # every pair closed and removed
-
 
 # (my solution)
 # time: O(n^2) because string appends can add up
